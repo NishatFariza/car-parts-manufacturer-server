333e22f81ff0d2042cd409c0fe20054def3d982e
NishatFariza <nishatfarzia78@gmail.com>
2022-05-23 16:43:32 +0600

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = process.env
+const port = process.env.PORT || 5000
+
+//user--facmasterFactory
+//pass--Zfg8KwNWMOviWg7z
+
+//connect to database
+const uri = `mongodb+srv://${process.env.DB_USER}:<password>@cluster0.8oq1d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Facmaster Factory server!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Facmaster Factory listening on port ${port}`)
 })
