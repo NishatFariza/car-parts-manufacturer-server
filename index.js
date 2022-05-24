43c50a4bea291a877afe915407b5f65727ecfde8
NishatFariza <nishatfarzia78@gmail.com>
2022-05-24 11:14:11 +0600

    product all data load

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
 //user--facmasterFactory
 //pass--Zfg8KwNWMOviWg7z
 
+//middleware
+app.use(cors());
+app.use(express.json())
+
 //connect to database
-const uri = `mongodb+srv://${process.env.DB_USER}:<password>@cluster0.8oq1d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8oq1d.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+   try{
+    await client.connect()
+    const productCollection = client.db("facmaster-factory").collection("product");
+    //  console.log('database connect');
+
+    
+      //6 product load
+      app.get('/products', async(req, res) =>{
+        const query ={}
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        // console.log(cars);
+        res.send(products)
+    })
+
+
+   }
+   finally{
+       
+   }
+}
+
+run().catch(console.dir)
+
 
 
 app.get('/', (req, res) => {
