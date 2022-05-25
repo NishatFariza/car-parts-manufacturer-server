3ada8bd4f3901df288ea08a91aaea9e4c13dc791
NishatFariza <nishatfarzia78@gmail.com>
2022-05-25 23:45:36 +0600

    create user collection api

--- index.js
+++ index.js
@@ -6,12 +6,31 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
-//user--facmasterFactory
-//pass--Zfg8KwNWMOviWg7z
+
 
 //middleware
 app.use(cors());
 app.use(express.json())
+
+
+//jwt create
+function verifyJwt(req, res, next) {
+  const authorize = req.headers.authorization;
+  if (!authorize) {
+      return res.status(401).send({ message: "Unauthorize access!" })
+  }
+  // console.log(authorize);
+  const token = authorize.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+      if (err) {
+          // console.log("err");
+          return res.status(403).send({ message: "Forbidden access" })
+      }
+      req.decoded = decoded;
+      next()
+  });
+}
+
 
 //connect to database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8oq1d.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,6 +41,7 @@ async function run(){
     await client.connect()
     const productCollection = client.db("facmaster-factory").collection("product");
     const reviewCollection = client.db("facmaster-factory").collection("review");
+    const userCollection = client.db("facmaster-factory").collection("user");
     //  console.log('database connect');
 
     
@@ -47,13 +67,27 @@ async function run(){
       const product = req.body;
       const result = await productCollection.insertOne(product)
       if (result.insertedId) {
-          res.send({success: true, message:`Successfuly Added ${product.name}`})
+          res.send({success: true, message:`Successfully Added ${product.name}`})
       }
       else{
           res.send({success: false, message:`Something is Wrong`})
       }
 
   })
+
+   // create user
+   app.put('/user/:email', async (req, res) => {
+    const user = req.body;
+    const email = req.params.email;
+    const filter = { email: email }
+    const options = { upsert: true };
+    const updatedDoc = {
+        $set: user
+    }
+    const result = await userCollection.updateOne(filter, updatedDoc, options);
+    const token = jwt.sign({ email: email }, process.env.SECRET_KEY, { expiresIn: '60d' });
+    res.send({ result, token })
+})
 
 
    }
